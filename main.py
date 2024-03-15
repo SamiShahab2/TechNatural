7f29151d6bfc0ee1a81d0a23caf10d9a0bbb28d9
samishahab <31791422-samishahab@users.noreply.replit.com>
2024-03-15 15:30:13 +0000

    Added to the dictionary for floor1 of the lab, finished over half of them

--- main.py
+++ main.py
@@ -8,6 +8,78 @@
 '''A gridbased text-adventure rpg'''
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Lists ========================================================================================
+LabFloor1={
+    "Destroyed Hive": {
+        "x": 4, 
+        "y": 3, 
+        "item": "N/A",
+        "enemy": "Metallic Bee",
+        "description": 
+        """Before you lies what seems to be the remains of a beehive.  
+Pieces of sharp metal and wax litter the room. Suddenly, a piece of shrapnel 
+flies past your head, barely missing you!""" 
+    },
+    "Overgrown Hallway": {
+        "x": 4,
+        "y": 2,
+        "item": "N/A",
+        "enemy": "N/A",
+        "description":
+        """You find yourself in a hallway filled with lush foliage. The walls
+and ground are covered in vines and greenery while the roof is bursting with
+flowers, each with a pleasant aroma."""
+    },
+    "Weapon Testing Chamber": {
+        "x": 4,
+        "y": 1,
+        "item": "N/A",
+        "enemy": "Rocket Ram",
+        "description":
+        """As you enter the room, smokey, debris filled air enters your lungs.
+It seems as if this was an area to test out weapons, though it has been destroyed.
+In the middle of the room stands one ram, seemingly the culprit of this destruction.
+Suddenly, a rocket launcher emerges from a panel on the ram's side!"""
+    },
+    "Stairs": {
+        "x": 3,
+        "y": 3,
+        "item": "N/A",
+        "enemy": "N/A",
+        "description":
+        """A staircase stands before you, leading up into the next room.
+There is a sweet, flowery aroma creeping down from the upper floor.
+You can choose to move up the staircase or stay in this floor for a bit longer."""
+    },
+    "Experiment Room": {
+        "x": 3,
+        "y": 2,
+        "item": "N/A",
+        "enemy": "Lazeep",
+        "description":
+        """Tools for welding and surgery litter this room. It appears they were
+merging animals and technology together in this location.
+A laser fired in your direction proves that these efforts weren't fruitless"""
+    },
+    "Equipment Room": {
+        "x": 3,
+        "y": 1,
+        "item": "Weak Paralyzer",
+        "enemy": "N/A",
+        "description":
+        """This room seems to have contained securtity equipment, though it has been
+ransacked and mostly cleared out. Empty panels once containing armour and
+weapons are sprawled across the walls, all open. Still, maybe there is something
+of use to be found?"""
+    },
+    "Documentation Room": {
+        "x": 2,
+        "y": 3,
+        "item": "N/A",
+        "enemy": "N/A",
+        "description":
+        """You nearly trip over a fallen over file cabinet while entering the room
+"""
+}
 
 # Imports and Global Variables =================================================================
 
